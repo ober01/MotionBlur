092b3e04a168e499fa59090f47a9bc3663021234
Alex.k <Alex.k@ramotion.com>
2016-11-26 14:12:42 +0300

    shared scheme

---
+++ Example/MotionBlurFramework/MotionBlurFramework.h
@@ -0,0 +1,19 @@
+//
+//  MotionBlurFramework.h
+//  MotionBlurFramework
+//
+//  Created by Alex K. on 26/11/16.
+//  Copyright © 2016 Arkadiusz Holko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MotionBlurFramework.
+FOUNDATION_EXPORT double MotionBlurFrameworkVersionNumber;
+
+//! Project version string for MotionBlurFramework.
+FOUNDATION_EXPORT const unsigned char MotionBlurFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MotionBlurFramework/PublicHeader.h>
+
+
